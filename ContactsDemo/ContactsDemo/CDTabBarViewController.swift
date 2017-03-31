cf772fb7d4777e76aba6ebb67837e4c0664b3585
tunghee <lth@lth-2.local>
2017-04-01 07:58:20 +0800

    init frame

---
+++ ContactsDemo/ContactsDemo/CDTabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CDTabBarViewController.swift
+//  ContactsDemo
+//
+//  Created by lth on 2017/3/31.
+//  Copyright © 2017年 lth. All rights reserved.
+//
+
+import UIKit
+
+class CDTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
